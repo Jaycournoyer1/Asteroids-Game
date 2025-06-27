09a5d84b988767973aa90e7228fa74de86b1fbd5
Jay Cournoyer <jaycournoyer@MacBookPro.lan>
2025-06-27 14:07:23 -0400

    Added additional arrow key inputs for movement

--- main.py
+++ main.py
@@ -6,55 +6,55 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
-    
+
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
-    
-    
+
     Player.containers = (updatable, drawable)
-    
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-    
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     dt = 0
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        
+
         updatable.update(dt)
-        
+
         for asteroid in asteroids:
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
-                
+
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
                     asteroid.split()
-                
+
         screen.fill("black")
-        
+
         for obj in drawable:
             obj.draw(screen)
-            
+
         pygame.display.flip()
-        
+
         # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000  
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
